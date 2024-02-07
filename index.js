6112b796f8e3c560306f487e77311735b7b891ef
moumen <moumen@gmail.com>
2024-02-07 13:05:57 +0100

    deleted

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const fs = require('node:fs')
-fs.readFile('index.txt','utf-8',(err,data)=>{
-console.log(data)
-})
+const fs = require("node:fs");
+
+fs.readFile("index.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+
+fs.unlink("mouman.txt", (err) => {
+  console.log("File deleted successfully");
+});
